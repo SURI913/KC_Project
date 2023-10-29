bfe11ed81c6778f1fdf4fb6c4db8b97e932f3ead
hayum0525 <khyoung9128@gmail.com>
2023-10-29 23:21:43 +0900

    던전 스테이지 화면

--- Assets/KHY/Script/DataM/MonsterData.cs
+++ Assets/KHY/Script/DataM/MonsterData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AllUnit;
 
 [System.Serializable]
 public class MonsterD
@@ -9,9 +10,11 @@ public class MonsterD
     public string stageID;
     public double hp;
     public double attack;
-    public double atktime;
+    public int atktime;
     public double recommattack;
     public double recommdefense;
+
+   
 }
 
 
@@ -19,23 +22,7 @@ public class MonsterD
 public class MonsterData : ScriptableObject
 {
     public MonsterD[] monsterdatas;
-   
-    /*[SerializeField]
-    private string stageID;
-    public string StageID { get { return stageID; } }
-    [SerializeField]
-    private float hp;
-    public float Hp { get { return hp; } }
-    [SerializeField]
-    private float attack;
-    public float Attack { get { return attack; } }
-    [SerializeField]
-    private float atkTime;
-    public float Atktime { get { return atkTime; } }
-    [SerializeField]
-    private float recommattack;
-    public float Recommattack { get { return recommattack; } }
-    [SerializeField]
-    private float recommdefense;
-    public float Recommdefense { get { return recommdefense; } }*/
+
+
+    
 }
