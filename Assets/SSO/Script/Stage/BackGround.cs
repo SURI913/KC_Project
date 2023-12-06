8f1ab1f3711bfc11218c07d0ed4ac217440880c9
tenbaby <110656858+tenbaby@users.noreply.github.com>
2023-12-07 05:05:41 +0900

    son
    son

---
+++ Assets/SSO/Script/Stage/BackGround.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BackGround : MonoBehaviour
+{
+    [SerializeField][Range(1f, 20f)] float speed = 3f;
+
+    private float width;
+    Vector2 startPos;
+    float newPos;
+
+    private void Start()
+    {
+        Renderer myRenderer = GetComponent<Renderer>();
+        width = myRenderer.bounds.size.x;
+
+        startPos = transform.position;
+        Debug.Log(width + "길이");
+    }
+
+    private void Update()
+    {
+        newPos = Mathf.Repeat(Time.time * speed, width);
+        transform.position = startPos + Vector2.left * newPos;
+    }
+}
