fb8794103db22dd17c26390ad708cd5517b13543
hayum0525 <khyoung9128@gmail.com>
2023-12-13 05:50:28 +0900

    Revert "Merge pull request #179 from SURI913/HY-1차-테스트"
    This reverts commit 34ecde7c7a14f869423aaa7e46505dc3ef134508, reversing
    changes made to 922f410362ddcfd24d8ed09876500a9d8eaf0634.

---
+++ Assets/KHY/Script/UI/TimeSlider.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeSlider : MonoBehaviour
+{
+
+    [SerializeField]
+    private Slider timeSlider;
+    [SerializeField]
+    private float maxTime = 40.0f;
+    private float currentTime;
+    public bool stopTime = false;
+
+    void Start()
+    {
+        timeSlider.maxValue = maxTime;
+        timeSlider.value = currentTime;
+
+        StartTime();
+
+       /* 
+        UpdateTime();
+        StartCoroutine(Countdown());*/
+    }
+
+    void Update()
+    {
+        // 추가적인 업데이트 로직이 필요한 경우 여기에 작성
+    }
+
+  /*  private void UpdateTime()
+    {
+        timeSlider.value =(float)currentTime / maxTime;
+    }*/
+    public void StartTime()
+    {
+        StartCoroutine(Countdown());
+    }
+    private IEnumerator Countdown()
+    {
+        while(stopTime == false)
+        {
+            maxTime -= Time.deltaTime;
+            yield return new WaitForSeconds(0.001f);
+
+            if(maxTime<-0)
+            {
+                stopTime = true;
+            }
+            if(stopTime==false)
+            {
+                timeSlider.value = maxTime;
+            }
+        }    
+
+        // 카운트다운이 끝났을 때의 추가 작업을 수행하려면 여기에 작성
+        //로드씬 ,  이미지 
+    }
+}
+
