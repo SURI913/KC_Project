0ee3f2b5dd489502513697ef8ed4b8bd596b7856
SURI <101981952+SURI913@users.noreply.github.com>
2023-12-14 05:31:16 +0900

    씬 생성

--- Assets/LSG/Script/Manager/ScenesManager.cs
+++ Assets/LSG/Script/Manager/ScenesManager.cs
@@ -7,7 +7,20 @@ using UnityEngine.SceneManagement;
 public class ScenesManager : MonoBehaviour
 {
     //씬 관리
+    /// <summary>
+    ///  main ~ 1-1~1-6 ~Dungeon~TowerUI~MainUI~ TowerUI~Inventory
+    /// 
+    /// </summary>
     Stack<int> scenes;
+    int currentSceneIndex = 0;
+
+    private void Awake()
+    {
+        //처음에 생성할 씬
+        StartCoroutine(TransitionToNextStage());
+        StartCoroutine(ShowMainUI());
+
+    }
 
     IEnumerator ShowLodingScene()
     {
@@ -26,42 +39,39 @@ public class ScenesManager : MonoBehaviour
     public IEnumerator ShowTower()
     {
         yield return new WaitForSeconds(3f);  // 3초 대기
-        SceneManager.LoadSceneAsync("TowerUI", LoadSceneMode.Additive);  //비동기 씬 로드
-        SceneManager.UnloadSceneAsync("MainUI");  //비동기 씬 로드
+        SceneManager.LoadSceneAsync(10, LoadSceneMode.Additive);  //비동기 씬 로드
+        int tmp = scenes.Pop();
+        if(tmp == 9) { Debug.Log("정상적으로 UI해제"); }
+        scenes.Push(10);
+
 
     }
 
     public IEnumerator ShowMainUI()
     {
         yield return new WaitForSeconds(3f);  // 3초 대기
-        SceneManager.LoadSceneAsync("MainUI", LoadSceneMode.Additive);  //비동기 씬 로드
+        SceneManager.LoadSceneAsync(9, LoadSceneMode.Additive);  //비동기 씬 로드 메인 UI
+        SceneManager.LoadSceneAsync(11, LoadSceneMode.Single);  //비동기 씬 로드 인벤토리
+
         SceneManager.UnloadSceneAsync("TowerUI");  //비동기 씬 로드
+        int tmp = scenes.Pop();
+        if (tmp == 10) { Debug.Log("정상적으로 UI해제"); }
+        scenes.Push(9);
+        scenes.Push(11);
+
     }
 
     public IEnumerator ShowDungeonUI()
     {
         yield return new WaitForSeconds(3f);  // 3초 대기
-        SceneManager.LoadSceneAsync("Dungeon", LoadSceneMode.Single);  //비동기 씬 로드
+        SceneManager.LoadSceneAsync(8, LoadSceneMode.Single);  //비동기 씬 로드
         foreach(var go in scenes)
         {
             SceneManager.UnloadSceneAsync(go);  //비동기 씬 로드
 
         }
         scenes.Clear();
-        scenes.Push(99); //던전 번호 체크
-    }
-
-    public IEnumerator ShowInventoryUI()
-    {
-        yield return new WaitForSeconds(3f);  // 3초 대기
-        SceneManager.LoadSceneAsync("Inventory", LoadSceneMode.Single);  //비동기 씬 로드
-        foreach (var go in scenes)
-        {
-            SceneManager.UnloadSceneAsync(go);  //비동기 씬 로드
-
-        }
-        scenes.Clear();
-        scenes.Push(99); //번호 체크
+        scenes.Push(8); //던전 번호 체크
     }
 
 }
