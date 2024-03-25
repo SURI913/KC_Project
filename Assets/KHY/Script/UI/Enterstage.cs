7df87eadcce149f1599dcadbb69b84286aa003ba
hayum0525 <khyoung9128@gmail.com>
2024-03-26 03:02:38 +0900

    UI 에셋 추가

--- Assets/KHY/Script/UI/Enterstage.cs
+++ Assets/KHY/Script/UI/Enterstage.cs
@@ -6,29 +6,11 @@ using UnityEngine.SceneManagement;
 public class Enterstage : MonoBehaviour
 {
     public GameObject UI;
-    public GameObject UI2;
-    public CountTime time;
-    public GameObject image;
-    public GameObject sliderbar;
+
     public void OnClick()
     {
-        /*    SceneManager.LoadScene("Dungeon");
-           //스테이지 입장
-
-           //지금은 클릭했을때 setactive로 UI를 가린다.
-
-           //UI 스테이지랑 enter,eixt 버튼 
-           UI.SetActive(false);
-           UI2.SetActive(false);
-           time.StartCountdwon();//enter버튼이 눌리면 카운트 시작 
-                                 // image.SetActive(false);
-
-           sliderbar.SetActive(true);*/
-
        GameManager.instance.Dungeon_Start_UI(true);
-        
-        
-
+       Debug.Log("버튼 눌러짐");
     }
 }
 
